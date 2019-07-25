8bb66104d3d6796832cd2884b035e6d60f73d185
James Guy <james.guy@James-MB.local>
2019-07-25 16:20:34 +0200

    bugfix

--- service/azure-ml-service.py
+++ service/azure-ml-service.py
@@ -71,10 +71,10 @@ def run_ml():
 
     # get entities from request
     entities = request.get_json()
-
+    print(type(entities))
     entity_id = entities.get('_id')
     print(type(entity_id))
-    
+
     # create the response
     return Response(get_ML_result(entity_id, entities), mimetype='application/json')
 
